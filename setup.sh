7cb118726862e46b530ce682a084aca1b79c5d1a
gokul656 <kgokul656@gmail.com>
2024-02-26 18:21:31 +0530

    added conditional installtion

--- setup.sh
+++ setup.sh
@@ -8,16 +8,17 @@ sudo swapoff -a
 sudo apt update -y
 sudo apt install curl net-tools openssh-server apt-transport-https ca-certificates gpg containerd -y
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+if ! command -v "kubelet" &> /dev/null
+then
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+    sudo apt-get update
+    sudo apt-get install -y kubelet kubeadm kubectl
+    sudo apt-mark hold kubelet kubeadm kubectl
+fi
 
 cd scripts
-
-chmod +x ./*
 
 source ~/scripts/setup-k3s.sh
 source ~/scripts/setup-provider.sh
